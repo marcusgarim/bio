83b44a3853bc65257b1fafa52b356ef254276c56
marcusgarim <marcusgarim@gmail.com>
2025-04-10 12:40:56 -0300

    add-v1-mobile

---
+++ backup_v1/app.js
@@ -0,0 +1,9 @@
+
+// Animação
+const cards = document.querySelectorAll('.card');
+cards.forEach((card, index) => {
+  setTimeout(() => {
+    card.style.opacity = '1';
+    card.style.animationDelay = `${index * 0.2}s`; 
+  }, 100);
+});
